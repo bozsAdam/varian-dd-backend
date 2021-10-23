2e8e2f3a1dbe47decec777d139247956257f7a69
Adam Bozsoki <adam.bozsoki@emarsys.com>
2021-10-23 23:44:58 +0200

    Add logging

--- src/main/java/hu/dd/varianddbackend/controller/PatientController.java
+++ src/main/java/hu/dd/varianddbackend/controller/PatientController.java
@@ -3,6 +3,7 @@ package hu.dd.varianddbackend.controller;
 import hu.dd.varianddbackend.model.Patient;
 import hu.dd.varianddbackend.model.StatusReport;
 import hu.dd.varianddbackend.repository.PatientRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,6 +14,7 @@ import org.springframework.web.server.ResponseStatusException;
 import java.util.List;
 import java.util.function.Supplier;
 
+@Slf4j
 @RestController
 public class PatientController {
     @Autowired
@@ -21,18 +23,21 @@ public class PatientController {
     @GetMapping("/patient/list")
     public List<Patient> getAllPatients()
     {
+        log.info("getting all patients");
         return patientRepository.findAll();
     }
 
     @GetMapping("/patient/{id}")
     public Patient getPatientById(@PathVariable("id") Long id)
     {
+        log.info(String.format("getting patient with id: %s", id));
         return patientRepository.findById(id).orElseThrow(getRuntimeExceptionSupplier());
     }
 
     @GetMapping("/patient/{id}/statusreports")
     public List<StatusReport> getStatusReportsOfPatient(@PathVariable("id") Long id)
     {
+        log.info(String.format("getting all statusreports for patient: %s", id));
         Patient patient = patientRepository.findById(id).orElseThrow(getRuntimeExceptionSupplier());
         return patient.getStatusReports();
     }
--- src/main/java/hu/dd/varianddbackend/controller/StatusReportController.java
+++ src/main/java/hu/dd/varianddbackend/controller/StatusReportController.java
@@ -2,6 +2,7 @@ package hu.dd.varianddbackend.controller;
 
 import hu.dd.varianddbackend.model.StatusReport;
 import hu.dd.varianddbackend.repository.StatusReportRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -11,6 +12,7 @@ import java.util.Date;
 import java.util.List;
 
 @RestController
+@Slf4j
 public class StatusReportController {
     @Autowired
     private StatusReportRepository statusRepository;
@@ -18,12 +20,14 @@ public class StatusReportController {
     @GetMapping("/statusreport/list")
     public List<StatusReport> getAllStatusReports()
     {
+        log.info("Listing all status reports");
         return statusRepository.findAll();
     }
 
     @GetMapping("/statusreport/{id}")
     public StatusReport getStatusReport(@PathVariable("id") Long id)
     {
+        log.info(String.format("Showing statusreport with id: %s", id));
         return statusRepository.findById(id).orElseThrow(()-> {
             throw new ResponseStatusException(
                     HttpStatus.NOT_FOUND, "Patient with this id does not exist"
@@ -34,6 +38,8 @@ public class StatusReportController {
     @PostMapping("/statusreport/add")
     public void addStatusReport(@RequestBody StatusReport statusReport)
     {
+        log.info("saving statusreport");
+        log.debug(statusReport.toString());
         statusReport.setDate(new Date(System.currentTimeMillis()));
         statusRepository.save(statusReport);
     }
