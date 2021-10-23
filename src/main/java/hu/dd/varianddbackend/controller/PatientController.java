14460338414da52eaa87c961f26a6bdc42f45df9
Adam Bozsoki <adam.bozsoki@emarsys.com>
2021-10-23 11:49:26 +0200

    Add patientrepository and controller with ability to get patient and list all patients

---
+++ src/main/java/hu/dd/varianddbackend/controller/PatientController.java
@@ -0,0 +1,35 @@
+package hu.dd.varianddbackend.controller;
+
+import hu.dd.varianddbackend.model.Patient;
+import hu.dd.varianddbackend.repository.PatientRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+
+@RestController
+public class PatientController {
+    @Autowired
+    private PatientRepository patientRepository;
+
+    @GetMapping("/patient/list")
+    public List<Patient> getAllPatients()
+    {
+        return patientRepository.findAll();
+    }
+
+    @GetMapping("/patient/{id}")
+    public Patient getPatientById(@PathVariable("id") Integer id)
+    {
+        return patientRepository.findById(id).orElseThrow(()-> {
+            throw new ResponseStatusException(
+                    HttpStatus.NOT_FOUND, "Patient with this id does not exist"
+            );
+        });
+    }
+
+}
---
+++ src/main/java/hu/dd/varianddbackend/repository/PatientRepository.java
@@ -0,0 +1,7 @@
+package hu.dd.varianddbackend.repository;
+
+import hu.dd.varianddbackend.model.Patient;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PatientRepository extends JpaRepository<Patient, Integer> {
+}
