ae6c7dee4b3a18c74d13ad0bad10847d3e5dc7f5
Adam Bozsoki <adam.bozsoki@emarsys.com>
2021-10-23 16:15:45 +0200

    Add StatusReport related things, now you can get, add status reports

---
+++ src/main/java/hu/dd/varianddbackend/model/StatusReport.java
@@ -0,0 +1,37 @@
+package hu.dd.varianddbackend.model;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.Date;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+public class StatusReport {
+    @Id
+    @GeneratedValue
+    private long id;
+
+    @ManyToOne
+    @JsonManagedReference
+    private Patient patient;
+
+    private String image;
+    @ElementCollection
+    private List<String> symptomAreas;
+    @ElementCollection
+    private List<String> lifestyleAreas;
+
+    @JsonFormat(shape=JsonFormat.Shape.STRING, pattern="yyyy-MM-dd HH:mm:ss")
+    private Date date;
+    private Priority priority;
+}
